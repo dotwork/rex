6b9c8fbff99deb59d8ea03ee32878ef99db4a8ac
Carlos Velez <velez.carlos.a@gmail.com>
2016-11-11 20:38:45 -0600

    MASTER - initial commit. It's a mess.

---
+++ rexpression.py
@@ -0,0 +1,160 @@
+import re
+from collections import namedtuple
+
+
+########################################################################################################################
+RexTuple = namedtuple("RexTuple", "name value is_opener closers condition side_effect index")
+
+
+########################################################################################################################
+class Rexpression(object):
+
+    ####################################################################################################################
+    def __init__(self, rex, text=None):
+        self.rex = rex
+        self._digits_exact = False
+        self._last_expression = None
+        self._re = None
+        self.text = text
+        self._last_matched_expression = None
+        self._last_match = None
+        # self.closers = []
+        #
+        # self.digits_with_exact_length_closer = RexTuple(name="end_exact_range", value=",}", is_opener=False, closer=None, side_effect=None)
+        # self.group_closers = [
+        #     RexTuple(name="end_group", value=")", is_opener=False, closer=None, side_effect=None),
+        # ]
+        # self.exactly_closers = [
+        #     RexTuple(name="digits", value="\d", is_opener=False, closer=None, side_effect=None),
+        #     RexTuple(name="letters", value="[a-zA-Z]", is_opener=False, closer=None, side_effect=None)
+        # ]
+
+    ####################################################################################################################
+    @property
+    def re(self):
+        if self._re is None:
+            self._re = re
+        return self._re
+
+    ####################################################################################################################
+    def check_for_start_or_end_chars(self, element):
+        start_end_char_map = {
+            "(": ")",
+            "{": "}",
+            "[": "]",
+        }
+        start_chars = []
+        for start, end in start_end_char_map.items():
+            if start == element.value:
+                start_chars.append(element.value)
+                return
+
+            elif end == element.value:
+                if element.value == start_end_char_map[start_chars[-1]]:
+                    start_chars.pop()
+                    return
+                else:
+                    msg = "Got '{actual}'. Expected '{expected}'."
+                    expected_name = start_end_char_map[start_chars[-1]]
+                    msg = msg.format(actual=element.name, expected=expected_name)
+                    raise TypeError(msg)
+
+    ####################################################################################################################
+    def handle_opening_element(self, element):
+        self.closers.append(element.closers)
+        if element.side_effect:
+            element.side_effect()
+
+    ####################################################################################################################
+    def handle_closing_element(self, element):
+        self.closers.pop()
+        if element.side_effect:
+            element.side_effect()
+
+    ####################################################################################################################
+    def expression(self):
+        self.closers = []
+        expression = r""
+        for element in self.rex._expression:
+            if element.is_opener:
+                self.handle_opening_element(element)
+
+            elif self.closers:
+                if element == self.closers[-1] or self.closers[0].condition(element):
+                    self.handle_closing_element(element)
+
+            expression = r"{}{}".format(expression, element.value)
+
+            # raise Exception("This is failing because nothing is checking if _digits_exact is True"
+            #                 "and then closing the bracket if it is.")
+
+            # if element.name == "digits_with_exact_length":
+            #     self._digits_exact = True
+            #     expression = r"{}{}".format(expression, element.value)
+            #     continue
+            #
+            # self.check_for_start_or_end_chars(element)
+            #
+            # if self._digits_exact and \
+            #         (self.ends_with_digits(expression) is False or element == self.last()):
+            #     expression += element.value + r",}"
+            #     self._digits_exact = False
+            # else:
+            #     expression = r"{}{}".format(expression, element.value)
+
+        return expression
+
+    ####################################################################################################################
+    def first_match(self, num=None):
+        if all([self._last_match is not None,
+                self._last_matched_expression is not None,
+                self._last_matched_expression == self._expression]):
+            match = self._last_match
+        else:
+            self._last_match = re.search(self.compile(), self.text)
+            self._last_matched_expression = self._expression
+            match = self._last_match
+
+        if num is None:
+            return match
+        else:
+            return match.group(num)
+
+    ####################################################################################################################
+    @staticmethod
+    def is_digit(char):
+        try:
+            int(char)
+            return True
+        except ValueError:
+            return False
+
+    ####################################################################################################################
+    def ends_with_digits(self, expression=None):
+        expression = expression or self.rex._expression[-1].value
+        last = expression[-1]
+        return self.is_digit(last)
+
+    ####################################################################################################################
+    def compile(self):
+        return re.compile(self.expression())
+
+    ####################################################################################################################
+    def last(self):
+        return self.rex._expression[-1]
+
+    ####################################################################################################################
+    def ends_with_open_curly_brace(self):
+        return self.last().value == "{"
+
+    ####################################################################################################################
+    def in_number_range(self):
+        return self.ends_with_digits() or self.ends_with_open_curly_brace()
+
+    ####################################################################################################################
+    def set_digits_exact_to_true(self):
+        self._digits_exact = True
+
+    ####################################################################################################################
+    def insert_digits_symbol(self, expression):
+        return "{expression}{digits}".format(expression=expression, digits="\d")
---
+++ test/test_rex.py
@@ -0,0 +1,153 @@
+import re
+import unittest
+from rex_writer import Rex
+
+
+########################################################################################################################
+class TestReWrite(unittest.TestCase):
+
+    ####################################################################################################################
+    def test_write(self):
+        # set an initial value for expression
+        rex = Rex.write.b.l.a.h
+        self.assertEqual("blah", rex.expression())
+
+        # write property should overwrite _expression to an empty list
+        rex.write
+        self.assertEqual([], rex._expression)
+
+        # expression method should return _expression attribute
+        self.assertEqual("", rex.expression())
+
+        # since each property returns 'self', calling more will
+        # append more characters to _expression/expression()
+        carlos = "Carlos"
+        rex2 = rex.C.a.r.l.o.s
+        self.assertEqual(carlos, rex.expression())
+        self.assertEqual(carlos, rex2.expression())
+
+        # if 'write' property is called_expression should be reset to empty string
+        # making more calls to properties after that will result in a new expression
+        rex3 = rex.write.s.c.a.r
+        self.assertEqual("scar", rex.expression())
+
+        # each rex object should be the same
+        self.assertEquals(rex, rex2, rex3)
+
+    ####################################################################################################################
+    def test_5_digit_zip_code(self):
+        expected_expression = r"\d{5,}"
+        pattern = re.compile(expected_expression)
+
+        zip_code = "73139"
+        text_with_zip_code = "blah{} blah".format(zip_code)
+
+        result = re.search(pattern, text_with_zip_code).group()
+        self.assertEqual(zip_code, result)
+
+        rex = Rex.write.exactly._5.digits
+        expression = rex.expression()
+        self.assertEqual(expected_expression, expression)
+
+        result = re.search(rex.compile(), text_with_zip_code).group()
+        self.assertEqual(zip_code, result)
+
+    ####################################################################################################################
+    def test_phone_number_pattern__with_dashes(self):
+        expected_expression = r"(\d{3,}\-\d{3,}\-\d{4,})"
+        pattern = re.compile(expected_expression)
+
+        rex = Rex.write.phone_number_pattern(dashes=True)
+        expression = rex.expression()
+        self.assertEqual(expected_expression, expression)
+
+        phone_number = "405-867-5309"
+        text_with_phone_number = "blah{} blah 723".format(phone_number)
+
+        result = re.search(pattern, text_with_phone_number).group()
+        self.assertEqual(phone_number, result)
+
+        result = re.search(rex.compile(), text_with_phone_number).group()
+        self.assertEqual(phone_number, result)
+
+    ####################################################################################################################
+    def test_phone_number_pattern__with_dots(self):
+        expected_expression = r"(\d{3,}\.\d{3,}\.\d{4,})"
+        pattern = re.compile(expected_expression)
+
+        rex = Rex.write.phone_number_pattern(dots=True)
+        self.assertEqual(expected_expression, rex.expression())
+
+        phone_number = "405.867.5309"
+        text_with_phone_number = "blah{} blah 723".format(phone_number)
+
+        result = re.search(pattern, text_with_phone_number).group()
+        self.assertEqual(phone_number, result)
+
+        result = re.search(rex.compile(), text_with_phone_number).group()
+        self.assertEqual(phone_number, result)
+
+    ####################################################################################################################
+    def test_phone_number_pattern__with_parenthesis(self):
+        phone_number = "(405) 867-5309"
+        text_with_phone_number = "blah{} blah 723".format(phone_number)
+
+        expected_expression = r"(\(\d{3,}\)\s\d{3,}\-\d{4,})"
+        pattern = re.compile(expected_expression)
+
+        result = re.search(pattern, text_with_phone_number).group()
+        self.assertEqual(phone_number, result)
+
+        rex = Rex.write.phone_number_pattern(parenthesis=True)
+        self.assertEqual(expected_expression, rex.expression())
+
+        result = re.search(rex.compile(), text_with_phone_number).group()
+        self.assertEqual(phone_number, result)
+
+    ####################################################################################################################
+    def test_phone_number_pattern__extract_digits(self):
+        phone_number = "(405) 867-5309"
+        text_with_phone_number = "blah{} blah 723".format(phone_number)
+
+        expected_expression = r"\((\d{3,})\)\s(\d{3,})\-(\d{4,})"
+        pattern = re.compile(expected_expression)
+
+        match = re.search(pattern, text_with_phone_number)
+        self.assertEqual(phone_number, match.group())
+        self.assertEqual("405", match.group(1))
+        self.assertEqual("867", match.group(2))
+        self.assertEqual("5309", match.group(3))
+
+        rex = Rex.read(phone_number).write \
+            .open_parenthesis.group.digits_with_exact_length._3.end_group.close_parenthesis \
+            .single_space.group.digits_with_exact_length._3.end_group \
+            .dash.group.digits_with_exact_length._4.end_group
+
+        match = re.search(rex.compile(), text_with_phone_number)
+        self.assertEqual(phone_number, match.group())
+        self.assertEqual("405", match.group(1))
+        self.assertEqual("867", match.group(2))
+        self.assertEqual("5309", match.group(3))
+
+        self.assertEqual(phone_number, rex.first_match(0))
+        self.assertEqual("405", rex.first_match(1))
+        self.assertEqual("867", rex.first_match(2))
+        self.assertEqual("5309", rex.first_match(3))
+
+    ####################################################################################################################
+    def test_phone_(self):
+        expected_expression = r"(\d{3,}\-\d{3,}\-\d{4,})"
+        pattern = re.compile(expected_expression)
+
+        rex = Rex.write.group\
+            .exactly._3.digits\
+            .dash.exactly._3.digits\
+            .dash.exactly._4.digits\
+            .end_group
+
+        expression = rex.expression()
+        self.assertEqual(expected_expression, expression)
+
+    ####################################################################################################################
+    def test_(self):
+        self.assertEqual(("blah", ), Rex.add_rex_tuple.closer)
