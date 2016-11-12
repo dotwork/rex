fc43398ceaabec0961e525154804894d1c764a5e
Carlos Velez <velez.carlos.a@gmail.com>
2016-11-11 20:51:42 -0600

    MASTER - add .gitignore file and start rewriting models with a tree structure in mind.

---
+++ models.py
@@ -0,0 +1,9 @@
+
+
+########################################################################################################################
+class RexWriter(object):
+    """Builds Rexpressions and stores them as a tree data structure on
+    the attribute 'root'.
+    """
+
+
